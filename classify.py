f820713994001b3279c45c86a378dd58258a6e6a
David Fifield <david@bamsoftware.com>
2015-07-13 15:54:13 -0700

    Add a classifer for 403-BADBEHAVIOR.

--- classify.py
+++ classify.py
@@ -31,6 +31,8 @@ def classify_response(response):
             return True, "403-GOOGLE-SORRY"
         if re.search("This IP has been automatically blocked\\.\nIf you have questions, please email: blocks-\\w+@craigslist\\.org\n", body):
             return True, "403-CRAIGSLIST"
+        if re.search("<p>You can use this key to <a href=\"http://www\\.ioerror\\.us/bb2-support-key\\?key=[\\w-]+\">fix this problem yourself</a>\\.</p>", body):
+            return True, "403-BADBEHAVIOR"
     if status == 403 or status == 404:
         if server == "AkamaiGHost" and re.search("<H1>Access Denied</H1>\n \nYou don't have permission to access \"[^\"]*\" on this server\\.<P>\nReference&#32;&#35;", body):
             return True, "%d-AKAMAI" % status
