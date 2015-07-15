9e28f81fb1b5e8f13ee98b0e3cb4fce0d61f0e82
David Fifield <david@bamsoftware.com>
2015-07-14 18:16:50 -0700

    Consider time of day in graph.R.

--- graph.R
+++ graph.R
@@ -3,11 +3,12 @@ library(ggplot2)
 palette <- c(UNBLOCKED="gray85", "TOR-BLOCKED"="blue", "NONTOR-BLOCKED"="darkorange", "BOTH-BLOCKED"="brown")
 
 x <- read.csv("findblocks.csv", colClasses=list(
-	date="Date", report_id="factor", probe_cc="factor",
+	report_id="factor", probe_cc="factor",
 	nontor_isblocked="logical", nontor_status="integer", nontor_class="factor",
 	tor_isblocked="logical", tor_status="integer", tor_class="factor",
 	url="factor"
 ))
+x$date <- as.POSIXct(x$date, tz="GMT")
 
 x$blocked <- ifelse(x$nontor_isblocked,
 	ifelse(x$tor_isblocked, "BOTH-BLOCKED", "NONTOR-BLOCKED"),
@@ -18,7 +19,7 @@ x$url <- reorder(x$url, x$url, length)
 x$url <- reorder(x$url, x$blocked=="TOR-BLOCKED", sum)
 
 p <- ggplot(x, aes(date, url, color=blocked))
-p <- p + geom_point(size=3, alpha=0.5, position=position_jitter(width=0.4, height=0))
+p <- p + geom_point(size=3, alpha=0.5, position=position_jitter(width=0.1, height=0))
 p <- p + guides(color=guide_legend(override.aes=c(alpha=1)))
 p <- p + scale_color_manual(values=palette)
 p <- p + scale_y_discrete(labels=function(l) {strtrim(l, 40)})
