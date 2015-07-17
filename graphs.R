ec2cb3e1ec4882f713979ce8a7961a0e8a368622
David Fifield <david@bamsoftware.com>
2015-07-16 17:11:13 -0700

    Try making a tor-blocked-type graph.

--- graphs.R
+++ graphs.R
@@ -51,3 +51,42 @@ p <- makegraph(x, NULL, "OONI URLs sorted by frequency of non-Tor blocking (Tor 
 ggsave("ooni-nontor-blocked.pdf", p, width=12, height=400, limitsize=F)
 p <- makegraph(x, 100, "OONI URLs sorted by frequency of non-Tor blocking (Tor is unblocked, non-Tor is blocked).")
 ggsave("ooni-nontor-blocked-top-100.pdf", p, width=12, height=12, limitsize=F)
+
+levels(x$tor_class) <- list(
+	"Akamai"=c("403-AKAMAI", "404-AKAMAI"),
+	"Amazon"=c("503-AMAZON"),
+	"Bluehost"=c("403-BLUEHOST"),
+	"CloudFlare"=c("403-CLOUDFLARE", "503-CLOUDFLARE"),
+	"Convio"=c("501-CONVIO"),
+	"Craigslist"=c("403-CRAIGSLIST"),
+	"Google"=c("403-GOOGLE-SORRY"),
+	"Site5"=c("406-SITE5"),
+	"SonicWALL"=c("403-SONICWALL"),
+	"Yelp"=c("503-YELP"),
+	"other"=c("403-OTHER", "404-OTHER", "405-OTHER", "406-OTHER", "500-OTHER", "502-OTHER", "503-DOD", "503-OTHER")
+)
+
+palette <- c(
+	"Akamai"="#0098cc",
+	"Amazon"="#232f3e",
+	"Bluehost"="#3575c0",
+	"CloudFlare"="#fe8d1b",
+	"Convio"="#470e6d",
+	"Craigslist"="#800080",
+	"Google"="#202020",
+	"Site5"="#f93c74",
+	"SonicWALL"="green",
+	"Yelp"="#c41200",
+	"other"="#202020"
+)
+
+x$url <- reorder(x$url, x$blocked, length)
+x$url <- reorder(x$url, x$blocked=="TOR-BLOCKED", sum)
+p <- ggplot(x[x$blocked=="TOR-BLOCKED" & x$url %in% rev(levels(x$url))[1:100], ], aes(date, url, color=tor_class))
+p <- p + geom_point(size=2.5, alpha=0.5)
+p <- p + guides(color=guide_legend(override.aes=c(alpha=1)))
+p <- p + scale_color_manual(values=palette)
+p <- p + scale_y_discrete(labels=function(l) {strtrim(l, 40)})
+p <- p + theme_bw()
+p <- p + labs(title="Tor blocking type", x=NULL, y=NULL)
+ggsave("tor-blocked-type-top-100.pdf", p, width=12, height=12, limitsize=F)
